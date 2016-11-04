c36f85375204734fb868f8de678dced3d81457d4
Yuriy <Yuriy@WOLF>
2016-11-05 00:09:13 +0100

    MVVM Hierarchies and navigation

---
+++ MVVMHierarchiesDemo/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MVVMHierarchiesDemo.Views;
+using MVVMHierarchiesDemo.ViewModel;
+
+namespace MVVMHierarchiesDemo
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public MyICommand<string> NavCommand { get; private set; }
+        private CustomerListViewModel custListViewModel = new CustomerListViewModel();
+        private OrderViewModel orderViewModel = new OrderViewModel();
+        private BindableBase _CurrentViewModel;
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _CurrentViewModel; }
+            set { SetProperty(ref _CurrentViewModel, value); }
+        }
+
+        private void OnNav(string destination)
+        {
+            switch (destination)
+            {
+                case "orders":
+                    CurrentViewModel = orderViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = custListViewModel;
+                    break;
+            }
+        }
+    }
+}
